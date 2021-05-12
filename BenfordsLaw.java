cb4dc45fc2c3caf4d6da6a88fb54955444b9a6c6
samatoki <81175009+samatoki@users.noreply.github.com>
2021-05-12 22:23:07 +0800

    Update BenfordsLaw.java

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -1,5 +1,5 @@
 /*
- * Name: Yuyang Liu, Jeffery Lin
+ * Name: Yuyang Liu, Jeffrey Lin
  * Date: May 5
  * Teacher: Mr. Ho
  * Description:
@@ -13,6 +13,35 @@ import java.io.FileNotFoundException;
 public class BenfordsLaw{
     public static void main(String[] args) throws FileNotFoundException
     {
+        // Initialize variables
+        Scanner customer = new Scanner(System.in);
+        String userInput, reportSalesOption, checkFraudOption, exitCondition;
+        reportSalesOption = "3";
+        checkFraudOption = "4";
+        exitCondition = "9";
+
+        do{
+            printMenu();
+            userInput = customer.nextLine();
+            
+            if (userInput.equals(reportSalesOption)){
+                loadFile();
+            }
+            
+        }while (!userInput.equals(exitCondition));
+
+        customer.close();
+    }
+
+    public static void printMenu(){
+        System.out.println("Customer and Sales System\n"
+        .concat("3. Report on total Sales\n")
+        .concat("4. Check for fraud in sales data\n")
+        .concat("9. Quit\n")
+        );
+    }
+    
+    public static void loadFile() throws FileNotFoundException{
         // Open File
         String fileName = "sales.csv";
         // Loading the file into the program
@@ -31,6 +60,8 @@ public class BenfordsLaw{
         }
 
         System.out.println(salesArr);
+        System.out.println();
         reader.close();
     }
+        
 }
