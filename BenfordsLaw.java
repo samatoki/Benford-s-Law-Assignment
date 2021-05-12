519645f925788eddafd08b16f9a5ac91fe81ff83
samatoki <81175009+samatoki@users.noreply.github.com>
2021-05-12 21:52:30 +0800

    Update BenfordsLaw.java

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -1,10 +1,11 @@
 /*
- * Name: Yuyang Liu
+ * Name: Yuyang Liu, Jeffery Lin
  * Date: May 5
  * Teacher: Mr. Ho
  * Description:
 */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -14,20 +15,22 @@ public class BenfordsLaw{
     {
         // Open File
         String fileName = "sales.csv";
-        //Loading the file into the program
+        // Loading the file into the program
         File saleFile = new File(fileName);
         
         // Read the file by creating Scanner instance to read the file in the java
         Scanner reader = new Scanner(saleFile);
-
         // Sets the delimiter pattern
         reader.useDelimiter(",");
+        // Create and initialize an arraylist to store the sales num
+        ArrayList<String> salesArr = new ArrayList<String>();
         
         while (reader.hasNext())
         {
-            System.out.print(reader.next());
+            salesArr.add(reader.next());
         }
-        
+
+        System.out.println(salesArr);
         reader.close();
     }
 }
