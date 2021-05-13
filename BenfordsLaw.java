63f45eb8c524435f679eb52d1ac9476934775d9b
samatoki <81175009+samatoki@users.noreply.github.com>
2021-05-13 12:05:19 +0800

    error version to edit

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -17,7 +17,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class Law{
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+import javafx.geometry.Side;
+import javafx.scene.layout.StackPane;
+
+public class BenfordsLaw{
     public static void main(String[] args) throws FileNotFoundException{
         // Initialize variables
         Scanner customer = new Scanner(System.in);
@@ -81,8 +91,7 @@ public class Law{
                     }
                 }
             }
-
-            
+            br.close();
         } catch (FileNotFoundException e){
             e.printStackTrace();
         } catch (IOException e){
@@ -100,11 +109,9 @@ public class Law{
         for(int i = 1; i <= 9; i++){
             total += counters[i];
         }
-
+        
         for(int i = 1; i <= 9; i++){
             frequency[i] = (counters[i]*100) / total;
-            System.out.println("The distribution of first digit " + i + " is " + counters[i]);
-            System.out.println("The distribution of first digit " + i + " is approximately " + frequency + " percent.");
         }
 
         if((frequency[1] > 29.0) && (frequency[1] < 32.0)){
@@ -114,12 +121,13 @@ public class Law{
             System.out.println("The data indicates that fraud is highly likely to have occurred.");
         }
 
-        String filepath = "results.csv";
-        saveRecord(frequency, filepath);
         
+        String filepath = "results.csv";
+        //saveRecord(frequency, filepath);
+        reportResults(frequency);
     }
 
-    public static void saveRecord(double[] frequency, String filepath){
+    /*public static void saveRecord(double[] frequency, String filepath){
         String percentage = "%";
         String num = "Number";
         
@@ -141,5 +149,42 @@ public class Law{
         }catch(Exception E){
             JOptionPane.showMessageDialog(null, " Record not Saved");
         }
+    }*/
+
+    public static void reportResults(double[] frequency){
+        Stage stage = null;
+        // Creating X and Y axes
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        // Adding labels for the axes
+        xAxis.setLabel("Digit");
+        yAxis.setLabel("Percent(%)");
+
+        // Creating a Bar Chart
+        BarChart firstDigitDistribution = new BarChart<>(xAxis, yAxis);
+
+        // Preparing data for the bar chart
+        XYChart.Series[] series = new XYChart.Series[10];
+        for(int i = 1; i <= frequency.length; i++){
+            series[i].setName("1 = " + frequency[1]);
+            series[i].getData().add(new XYChart.Data(i, frequency[1]));
+        }
+        
+        // Setting the data to bar chart
+        firstDigitDistribution.getData().addAll(series[1], series[2], series[3], series[4], series[5], series[6], series[7], series[8], series[9]);
+
+        // Setting the legend on the top
+        firstDigitDistribution.setLegendSide(Side.RIGHT);
+
+        // Creating a stack pane to hold the chart
+        StackPane pane = new StackPane(firstDigitDistribution);
+
+        //Setting the Scene
+        Scene scene = new Scene(pane, 595, 300);
+        stage.setTitle("Bar Chart Of First Digit Distribution");
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
