002f24cf324d17930cb47915981c6ecfcc2762ee
TMDNTDL <81274608+TMDNTDL@users.noreply.github.com>
2021-05-13 10:14:37 -0400

    Update BenfordsLaw.java

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -77,7 +77,7 @@ public class BenfordsLaw{
     public static void loadFile(String userInput) throws FileNotFoundException{
         // Open File
         Scanner sc = new Scanner(System.in);
-        System.out.println("Type (sales.csv) file in order to import it:");
+        System.out.println("Type the name of sales data file (Ex: sales.csv) in order to import it:");
         String fileName = sc.nextLine();
 
         // Initialize a variable used to read the file line by line
